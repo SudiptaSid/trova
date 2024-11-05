b0fe6bd938f9d7647b19be5c71975e3eee1d41dd
Sudipta Biswas <sudipta.biswas.5648@gmail.com>
2024-11-05 23:42:47 +0530

    Initial commit

---
+++ js/default.js
@@ -0,0 +1,45 @@
+// ========== Banner Slider
+/*$('#banner_slider').owlCarousel({
+    margin:0,
+    items: 1,
+    dots:false,
+    thumbs:false,
+    nav:true,
+    navText : ["<span class='left_ctrl'></span>","<span class='right_ctrl'></span>"], 
+    loop:true,
+    smartSpeed:2000,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsiveClass:true, 
+    responsive: {
+        300: {
+            nav:false,
+        },
+        700: {
+            nav:true,
+        }
+    }
+});*/
+// ========== Thumbnail Slider
+/*$('#more').owlCarousel({
+    dots: false,
+    nav: false,
+    navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    loop: true,
+    margin: 10,
+    items: 4,
+    responsiveClass: true,
+    thumbs:false,
+    responsive: {
+      200: {
+        items:2,
+      },
+      400: {
+        items:3,
+      },
+      501: {
+        items:4,
+      },
+    }
+});*/
